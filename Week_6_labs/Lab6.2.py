803443d8e04a7791a56c7974ef86f6660fbe5e98
ShaunaB93 <35816763+ShaunaB93@users.noreply.github.com>
2019-12-18 22:44:04 +0000

    Initial version

---
+++ Week_6_labs/Lab6.2.py
@@ -0,0 +1,7 @@
+# Shauna Byrne
+# Code adapted from Lab instructions from Lab06.2 given by Andrew Beatty - GMIT - Computing & Data Analytics 2019
+# Write a python program that will read in a html page from a file and prints it out again 
+
+f = open("../../carviewer.html", "r")
+html = f.read()
+print (html)
