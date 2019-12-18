00903ea237c1e975bdad8963b47fb50ffa45d593
ShaunaB93 <35816763+ShaunaB93@users.noreply.github.com>
2019-12-18 23:46:24 +0000

    Version 2

--- Week7_Labs/Lab07.1.py
+++ Week7_Labs/Lab07.1.py
@@ -4,7 +4,7 @@
 
 import requests
 import json
-
+from xlwt import *
 
 #url = "https://reports.sem-o.com/api/v1/documents/static-reports"
 url= "https://reports.sem-o.com/api/v1/documents/static-reports?ReportName=Balancing%20and%20Imbalance%20Market%20Cost%20View&Date=>2019-11-08"
@@ -16,6 +16,16 @@ listOfReports = []
 for item in data["items"]:
     listOfReports.append(item["ResourceName"])
 
+w = Workbook()
+ws = w.add_sheet('items')
+rowNumber = 0;
+ws.write(rowNumber,0,"StartTime")
+ws.write(rowNumber,1,"EndTime")
+ws.write(rowNumber,2,"ImbalanceVolume")
+ws.write(rowNumber,3,"ImbalancePrice")
+ws.write(rowNumber,4,"ImbalanceCost")
+rowNumber += 1 
+
 for ReportName in listOfReports:
     url ="https://reports.sem-o.com/api/v1/documents/"+ReportName
     print(url)
@@ -23,6 +33,6 @@ for ReportName in listOfReports:
     aReport= response.json()
 
 # Saves data to a file
-filename = 'reports.json'
+filename = 'reports2.json'
 f =  open(filename, 'w')
 json.dump(data, f, indent=4)
