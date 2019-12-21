b09d0feea6eb157790969bae975c86663845b40b
ShaunaB93 <35816763+ShaunaB93@users.noreply.github.com>
2019-12-21 22:43:09 +0000

    Add files via upload

--- DraftsFinalProject/server1.py
+++ DraftsFinalProject/server1.py
@@ -1,5 +1,4 @@
-from flask import Flask
-from flask import jsonify
+from flask import Flask, jsonify, request, abort
 
 app = Flask(__name__, static_url_path='', static_folder='.')
 
@@ -37,17 +36,62 @@ def findID(id):
     
     return jsonify(filmCheck[0])
 
+#curl "http://127.0.0.1:5000/films/3"
+
 @app.route('/films', methods=['POST'])
 def createFilm():
-    return "in create film"
+    global nextID
+    if not request.json:
+        abort(400)
+    #Check that properly formatted add code here
+    film={
+        "ID":nextID,
+        "Title": request.json['Title'],
+        "Rating": request.json['Rating'],
+        "Director": request.json['Director']
+    }
+    nextID += 1
+    films.append(film)
+    return jsonify(film)
+#curl -i -H "Content-Type:application/json" -X POST -d "{\"Title\":\"King Kong\",\"Rating\":\"6\",\"Director\":\"Peter Jackson\"}" http://127.0.0.1:5000/films
 
 @app.route('/films/<int:id>', methods=['PUT'])
 def updateFilm(id):
-    return "in update for id " +str(id)
+    changeFilm = list(filter(lambda f: f['ID']==id, films))
+    if (len(changeFilm) ==0):
+        abort(404)
+    changeFilm = changeFilm[0]
+    
+    if not request.json:
+        abort(400)
+
+    requestJson = request.json
+    if 'Title' in requestJson and type(requestJson['Title']) is not str:
+        abort(400)
+    if 'Rating' in requestJson and type(requestJson['Rating']) is not int:
+        abort(400)
+    if 'Director' in requestJson and type(requestJson['Director']) is not str:
+        abort(400)
+    
+    if 'Title' in requestJson:
+        changeFilm['Title'] = requestJson['Title']
+    
+    if 'Rating' in requestJson:
+        changeFilm['Rating'] = requestJson['Rating']
+    
+    if 'Director' in requestJson:
+        changeFilm['Director'] = requestJson['Director']
+    
+    return jsonify(changeFilm)
+#curl -i -H "Content-Type:application/json" -X PUT -d "{\"Title\":\"King Kong\",\"Director\":\"Peter Jackson\",\"Rating\":3}" http://127.0.0.1:5000/films/1
 
 @app.route('/films/<int:id>', methods=['DELETE'])
 def deleteFilm(id):
-    return "in delete for id " +str(id)
+    deleteFilm = list(filter(lambda f: f['ID']==id, films))
+    if (len(deleteFilm) ==0):
+        abort(404)
+    films.remove(deleteFilm[0])
+    return jsonify({"done":True})
 
 if __name__ == '__main__':
     app.run(debug= True)
